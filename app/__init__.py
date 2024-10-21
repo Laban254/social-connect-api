1b00658888a5499fa31d6fa9bd88d2c32876ed01
laban254 <labanrotich6544@gmail.com>
2024-10-21 23:31:08 +0300

    Update secret key handling in create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,16 @@
 from flask import Flask
 from .auth.oauth import init_oauth
 from .auth.routes import auth_bp
+from app.config import Config
+
+config = Config()
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = 'your_secret_key'  # Replace with a strong secret key
-
-
+    app.secret_key = config.SECRET_KEY  
 
     init_oauth(app)
 
-    # Register blueprints
     app.register_blueprint(auth_bp)
 
     return app
