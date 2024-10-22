ca7161f09dbbd51b5dd700b60ec1edc8fcebbcab
laban254 <labanrotich6544@gmail.com>
2024-10-23 01:03:45 +0300

    Integrate SocketIO for real-time interactions

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from flask_socketio import SocketIO
 from .auth.oauth import init_oauth
 from .auth.routes import auth_bp
-from .users.routes import  users_bp
-from app.config import Config
-from app.extensions import db, migrate
+from .users.routes import users_bp
 from .posts.routes import posts_bp
 from .interactions.routes import interactions_bp
+from app.config import Config
+from app.extensions import db, migrate
+
+socketio = SocketIO()  # Initialize SocketIO
 
 config = Config()
 
@@ -20,11 +23,13 @@ def create_app():
 
     init_oauth(app)
 
+    # Register Blueprints
     app.register_blueprint(auth_bp)
     app.register_blueprint(users_bp)
     app.register_blueprint(posts_bp)
     app.register_blueprint(interactions_bp)
 
-
+    # Initialize SocketIO with the app
+    socketio.init_app(app)
 
     return app
